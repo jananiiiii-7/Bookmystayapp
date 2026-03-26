f6d87957c467bf12178b70f44bba52d5ae24c627
jananiiiii-7 <jananisreed@gmail.com>
2026-03-26 14:45:20 +0530

    UC1: Application Entry & Welcome Message

---
+++ TrainConsistManagementApp/src/HotelBookingApp.java
@@ -0,0 +1,21 @@
+
+/**
+ * ==============================================================
+ * Book My Stay App
+ * UC1: Application Entry & Welcome Message
+ * ==============================================================
+ */
+
+public class HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("============================================");
+        System.out.println(" Welcome to the Hotel Booking Management System ");
+        System.out.println("============================================");
+
+        System.out.println("Application Name : Book My Stay App");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully.");
+    }
+}
